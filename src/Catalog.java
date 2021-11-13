6dc32b55f1f543201dc106664231efafe65ff363
Denis Avramenko <d.avramenko@tapston.com>
2021-11-14 01:00:09 +0300

    Initial commit

---
+++ src/Catalog.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+/**
+ * The type Catalog.
+ */
+public class Catalog {
+    private List<IProducts> productsList;
+
+    /**
+     * Instantiates a new Catalog.
+     *
+     * @param productsList the products list
+     */
+    public Catalog(List<IProducts> productsList) {
+        this.productsList = productsList;
+    }
+
+    /**
+     * Gets products list.
+     *
+     * @return the products list
+     */
+    public List<IProducts> getProductsList() {
+        return productsList;
+    }
+}
